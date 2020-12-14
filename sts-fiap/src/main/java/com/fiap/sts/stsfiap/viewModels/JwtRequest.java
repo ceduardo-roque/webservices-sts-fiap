44c2aea4f8fd4c9244d61f6fe0d4ad40c9b0c23d
Carlos Eduardo Roque da Silva <ceduardo.roque@gmail.com>
2020-12-13 23:41:09 -0300

    Getters Setters

--- sts-fiap/src/main/java/com/fiap/sts/stsfiap/viewModels/JwtRequest.java
+++ sts-fiap/src/main/java/com/fiap/sts/stsfiap/viewModels/JwtRequest.java
@@ -6,4 +6,37 @@ public class JwtRequest {
     public String clientId;
     public String clientSecret;
     public String grantType;
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getClientId() {
+		return clientId;
+	}
+	public void setClientId(String clientId) {
+		this.clientId = clientId;
+	}
+	public String getClientSecret() {
+		return clientSecret;
+	}
+	public void setClientSecret(String clientSecret) {
+		this.clientSecret = clientSecret;
+	}
+	public String getGrantType() {
+		return grantType;
+	}
+	public void setGrantType(String grantType) {
+		this.grantType = grantType;
+	}
+    
+    
+    
 }
